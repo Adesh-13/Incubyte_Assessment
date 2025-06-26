683e070a7865806b04888c34077bf07d245eda48
Aadesh <adeshnewaskar@gmail.com>
2025-06-26 12:55:23 +0530

    Initial Commit

---
+++ Incubyte_Assessment/Incubyte_AssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Incubyte_AssessmentApp.swift
+//  Incubyte_Assessment
+//
+//  Created by Adesh Newaskar on 26/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Incubyte_AssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Incubyte_AssessmentTests/Incubyte_AssessmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  Incubyte_AssessmentTests.swift
+//  Incubyte_AssessmentTests
+//
+//  Created by Adesh Newaskar on 26/06/25.
+//
+
+import Testing
+@testable import Incubyte_Assessment
+
+struct Incubyte_AssessmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
